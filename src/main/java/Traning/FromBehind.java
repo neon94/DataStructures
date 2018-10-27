fb771a5ec1955fed24884c32808d69e5a5c4446f
Dawid Jaloza <dawidjaloza@gmail.com>
2018-10-27 11:00:14 +0200

    27.10.2018 1st

---
+++ src/main/java/Traning/FromBehind.java
@@ -0,0 +1,15 @@
+package Traning;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FromBehind {
+
+    public static void main(String[] args) {
+        List<Integer> chain = Arrays.asList(1,2,3,8,5,3,8);
+
+
+
+    }
+
+}
